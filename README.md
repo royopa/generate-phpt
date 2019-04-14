95142936c609abf50737c5ee3c95680a4fdbbfbf
Rodrigo Prado de Jesus <royopa@gmail.com>
2019-04-14 15:46:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# generate-phpt
+
+Tool to generate phpt files skeleton
+
+
+## Generating the phar file
+
+
+```shell
+$ ./create-phar.php
+```
+
+So the file `generate-phpt.phar` will be created on `build` folder.
---
+++ create-phar.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This creates a standalone phar file with all of the PHP source included. To run the
+ * phar just type 'php generate-phpt.phar <options>' at the command line.
+ */
+
+if (Phar::canWrite()) {
+	echo "Writing phar archive\n";
+} else {
+ 	echo "Unable to write archive, check that phar.readonly is 0 in your php.ini\n";
+ 	exit();
+}
+
+$thisDir = dirname(__FILE__);
+
+$pharPath = $thisDir . DIRECTORY_SEPARATOR . 'build';
+
+$pharFile = 'generate-phpt.phar';
+
+$phar = new Phar($pharPath . DIRECTORY_SEPARATOR . $pharFile);
+
+$phar->buildFromDirectory($thisDir . DIRECTORY_SEPARATOR . 'src');
+
+$stub = <<<ENDSTUB
+<?php
+Phar::mapPhar('generate-phpt.phar');
+require 'phar://generate-phpt.phar/generate-phpt.php';
+__HALT_COMPILER();
+ENDSTUB;
+
+$phar->setStub($stub);
+
+# Make the file executable
+chmod($pharPath . DIRECTORY_SEPARATOR . $pharFile, 0770);
+
+echo "$pharFile successfully created on build folder" . PHP_EOL;
